d3ae2e67c49df421bf068c28d17179f03c6c9220
dckmedia <charithaprr@gmail.com>
2016-10-29 22:15:32 +1100

    hg

---
+++ storyline/tmp1/story_content/user.js
@@ -0,0 +1,40 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6JObBykimoy":
+        Script1();
+        break;
+      case "6lieYPVJTDy":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player=GetPlayer();
+var notes = player.GetVar('StuNotes');
+var Name = player.GetVar('FullName1');
+var Unit = player.GetVar('UnitName');
+
+window.open('https://studyconnect.edu.au/printpdf/index.php?w1=' + Name + '&w2=' + Unit + '&w3=' + notes , '_blank' );
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var myName  = lmsAPI.GetStudentName();
+
+var myid  = lmsAPI.GetStudentID();
+
+
+var array = myName.split(',');
+var newName = array[1] + ' ' + array[0];
+
+player.SetVar("FullName1", newName);
+player.SetVar("StuId", myid);
+
+}
+
